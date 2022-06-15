eae7b322aa9f1d55f96b1726aa1658c0aeab62bb
Crowdbotics <team@crowdbotics.com>
2022-06-15 22:24:20 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "tropical_parrot_cou_35741",
+  url: "https://tropical_parrot_cou_35741.botics.co",
+  api: "https://tropical_parrot_cou_35741.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
